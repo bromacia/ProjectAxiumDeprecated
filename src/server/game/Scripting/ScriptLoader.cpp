c5f563a4dd207921a90522fa7f93f2fe56302a70
Ashley <saeshyls@gmail.com>
2012-03-04 01:59:52 +0100

    Profession NPC

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -1228,15 +1228,14 @@ void AddBattlegroundScripts()
 #endif
 }
 
+// Custom
 #ifdef SCRIPTS
-/* This is where custom scripts' loading functions should be declared. */
-
+    void AddSC_prof_npc();
 #endif
 
 void AddCustomScripts()
 {
 #ifdef SCRIPTS
-    /* This is where custom scripts should be added. */
-
+    AddSC_prof_npc();
 #endif
 }
