368ff5e87aec504da6bf49500bcb6b79a1379082
Ashley <saeshyls@gmail.com>
2012-03-04 02:35:16 +0100

    Increase Duel Zone radius

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -7499,7 +7499,7 @@ void Player::CheckDuelDistance(time_t currTime)
 
     if (duel->outOfBound == 0)
     {
-        if (!IsWithinDistInMap(obj, 50))
+        if (!IsWithinDistInMap(obj, 80))
         {
             duel->outOfBound = currTime;
 
@@ -7509,7 +7509,7 @@ void Player::CheckDuelDistance(time_t currTime)
     }
     else
     {
-        if (IsWithinDistInMap(obj, 40))
+        if (IsWithinDistInMap(obj, 80))
         {
             duel->outOfBound = 0;
 
