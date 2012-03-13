da530fba5cc84a8c4b988e20b63ee8865c43a01c
Ashley <saeshyls@gmail.com>
2012-03-13 00:24:30 -0400

    Revert "Update BG history"
    This reverts commit 6547364d2b96d37d72e58898c893a396d5a56f3d.

--- sql/Axium/world_bg_history.sql
+++
@@ -1,4 +0,0 @@
-REPLACE INTO `trinity_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES
-(11100, 'Guild win \'%s\' Total damage \'%s\'  Total heal \'%s\', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,'),
-(11101, 'Guild loose \'%s\' Total damage \'%s\'  Total heal \'%s\', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,'),
-(11102, 'Guild win \'%s\' and loose guild \'%s\' Total damage \'%s\'  Total heal \'%s\', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,'); 
