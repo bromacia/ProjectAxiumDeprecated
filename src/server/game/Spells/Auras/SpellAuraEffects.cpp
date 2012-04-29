eb82f0875b7b558a81de8f9ab568733874c0dc15
Ashley <saeshyls@gmail.com>
2012-04-29 15:35:00 -0400

    Bind Sight should be correctly removed when the target goes stealth or invis

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -1647,6 +1647,10 @@ void AuraEffect::HandleModInvisibility(AuraApplication const* aurApp, uint8 mode
         target->m_invisibility.AddFlag(type);
         target->m_invisibility.AddValue(type, GetAmount());
 
+        // Arena Spectator
+        // Remove Bind Sight
+        target->RemoveAurasByType(SPELL_AURA_BIND_SIGHT);
+
         UnitList targets;
         Trinity::AnyUnfriendlyUnitInObjectRangeCheck u_check(target, target, target->GetMap()->GetVisibilityRange());
         Trinity::UnitListSearcher<Trinity::AnyUnfriendlyUnitInObjectRangeCheck> searcher(target, targets, u_check);
@@ -1746,6 +1750,10 @@ void AuraEffect::HandleModStealth(AuraApplication const* aurApp, uint8 mode, boo
         target->SetStandFlags(UNIT_STAND_FLAGS_CREEP);
         if (target->GetTypeId() == TYPEID_PLAYER)
             target->SetByteFlag(PLAYER_FIELD_BYTES2, 3, PLAYER_FIELD_BYTE2_STEALTH);
+
+        // Arena Spectator
+        // Remove Bind Sight
+        target->RemoveAurasByType(SPELL_AURA_BIND_SIGHT);
 
         UnitList targets;
         Trinity::AnyUnfriendlyUnitInObjectRangeCheck u_check(target, target, target->GetMap()->GetVisibilityRange());
