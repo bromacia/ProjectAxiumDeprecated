b996f4cfa8eace8ec78402cd51106c1b9d60a268
Ashley <saeshyls@gmail.com>
2012-07-04 04:19:53 +0100

    Fix Master's Call being usable while the Hunter's pet is CCd

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -5597,8 +5597,8 @@ SpellCastResult Spell::CheckCast(bool strict)
         // Dont allow Master's Call to be cast if the hunter's pet is dead
         if (m_spellInfo->Id == 53271)
             if (Unit* pet = m_caster->ToPlayer()->GetPet())
-                if (pet->isDead())
-                    return SPELL_FAILED_TARGETS_DEAD;
+				if (pet->isDead() || pet->IsCrowdControlled())
+                    return SPELL_FAILED_CANT_DO_THAT_RIGHT_NOW;
 
         // Dont allow Stealth or Invisibility to be casted while the target is Flared
         if (m_spellInfo->Id == 1784 || m_spellInfo->Id == 66
