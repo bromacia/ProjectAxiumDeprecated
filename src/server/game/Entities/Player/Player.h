0d838d9ac94e9969fbc8bee9ace4d55ff469122d
Ashley <saeshyls@gmail.com>
2012-08-04 12:04:32 -0400

    Implement IsDueling() for future changes

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -1832,6 +1832,7 @@ class Player : public Unit, public GridObject<Player>
         void CheckDuelDistance(time_t currTime);
         void DuelComplete(DuelCompleteType type);
         void SendDuelCountdown(uint32 counter);
+        bool IsDueling() const { return duel != NULL; }
 
         bool IsGroupVisibleFor(Player const* p) const;
         bool IsInSameGroupWith(Player const* p) const;
