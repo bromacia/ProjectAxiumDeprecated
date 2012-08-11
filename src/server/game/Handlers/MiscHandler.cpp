7cc51676fa5f9fd7f5fde15bb5ff048a22c48ca4
Ashley <saeshyls@gmail.com>
2012-08-11 18:25:35 -0400

    Allow logout during duels and during jumping or falling

--- src/server/game/Handlers/MiscHandler.cpp
+++ src/server/game/Handlers/MiscHandler.cpp
@@ -404,10 +404,8 @@ void WorldSession::HandleLogoutRequestOpcode(WorldPacket & /*recv_data*/)
 
     if (GetPlayer()->isInCombat())
         reason = 1;
-    else if (GetPlayer()->m_movementInfo.HasMovementFlag(MOVEMENTFLAG_JUMPING | MOVEMENTFLAG_FALLING))
-        reason = 3;                                         // is jumping or falling
-    else if (GetPlayer()->duel || GetPlayer()->HasAura(9454)) // is dueling or frozen by GM via freeze command
-        reason = 2;                                         // FIXME - Need the correct value
+    else if (GetPlayer()->HasAura(9454)) // is dueling or frozen by GM via freeze command
+        reason = 2; // FIXME - Need the correct value
 
     if (reason)
     {
