f75ec5b78a178d05f1ffb965d1f6046ef40e7d90
takenbacon <revoke@darkblizz.org>
2013-07-13 04:32:23 -0700

    Fixed logic fail in dispel (not sure if this might break something, but this is still a logic fail)

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -3325,7 +3325,7 @@ void Spell::EffectDispel(SpellEffIndex effIndex)
             {
                 // do not remove positive auras if friendly target
                 //               negative auras if non-friendly target
-                if (aurApp->IsPositive() == unitTarget->IsFriendlyTo(m_caster))
+                if (aurApp->IsPositive() && unitTarget->IsFriendlyTo(m_caster))
                     continue;
             }
 
