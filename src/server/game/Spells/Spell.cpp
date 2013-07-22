7b8f7529ccd6b32c4e34cec062f25cc84f3dd3fe
Ashley <saeshyls@gmail.com>
2013-07-22 12:43:51 -0400

    Fix memory leak when Load() fails from spell/aura-scripts

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -7456,6 +7456,7 @@ void Spell::LoadScripts()
         {
             std::list<SpellScript*>::iterator bitr = itr;
             ++itr;
+            delete (*bitr);
             m_loadedScripts.erase(bitr);
             continue;
         }
