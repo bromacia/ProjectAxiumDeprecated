df972b8e619b2066709f90eec89afc2fd1f7ad88
Ashley <saeshyls@gmail.com>
2013-09-06 02:00:29 -0400

    Remove old hackfix for Swiftmend

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -2287,22 +2287,6 @@ void Spell::EffectHeal(SpellEffIndex /*effIndex*/)
                 tickcount = 4;
                 tickheal += (m_caster->ToPlayer()->GetUInt32Value(PLAYER_FIELD_MOD_HEALING_DONE_POS) * 0.1305);
                 tickheal = targetAura->GetCaster()->SpellHealingBonusDone(unitTarget, targetAura->GetSpellInfo(), tickheal, DOT);
-                if (caster->HasAura(17111))
-                    tickheal += tickheal * 0.05;
-                else if (caster->HasAura(17112))
-                    tickheal += tickheal * 0.10;
-                else if (caster->HasAura(17113))
-                    tickheal += tickheal * 0.15;
-                if (caster->HasAura(17104))
-                    tickheal += tickheal * 0.02;
-                else if (caster->HasAura(24943))
-                    tickheal += tickheal * 0.04;
-                else if (caster->HasAura(24944))
-                    tickheal += tickheal * 0.06;
-                else if (caster->HasAura(24945))
-                    tickheal += tickheal * 0.08;
-                else if (caster->HasAura(24946))
-                    tickheal += tickheal * 0.10;
             }
             // Regrowth
             else if (targetAura->GetSpellInfo()->SpellFamilyFlags[0] & 0x40)
@@ -2311,16 +2295,6 @@ void Spell::EffectHeal(SpellEffIndex /*effIndex*/)
                 tickheal += 55;
                 tickheal += (m_caster->ToPlayer()->GetUInt32Value(PLAYER_FIELD_MOD_HEALING_DONE_POS) * 0.09);
                 tickheal = targetAura->GetCaster()->SpellHealingBonusDone(unitTarget, targetAura->GetSpellInfo(), tickheal, DOT);
-                if (caster->HasAura(17104))
-                    tickheal += tickheal * 0.02;
-                else if (caster->HasAura(24943))
-                    tickheal += tickheal * 0.04;
-                else if (caster->HasAura(24944))
-                    tickheal += tickheal * 0.06;
-                else if (caster->HasAura(24945))
-                    tickheal += tickheal * 0.08;
-                else if (caster->HasAura(24946))
-                    tickheal += tickheal * 0.10;
             }
 
             addhealth += tickheal * tickcount;
