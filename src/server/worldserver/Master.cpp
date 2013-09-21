d324b76be77b2105186c9ae99445eea94cedb838
takenbacon <revoke@darkblizz.org>
2013-09-21 00:15:27 -0700

    Fixed previous commit

--- src/server/worldserver/Master.cpp
+++ src/server/worldserver/Master.cpp
@@ -466,10 +466,8 @@ void Master::_StopDB()
 void Master::clearOnlineAccounts()
 {
     // Cleanup online status for characters hosted at current realm
-    /// \todo Only accounts with characters logged on *this* realm should have online status reset. Move the online column from 'account' to 'realmcharacters'?
-    LoginDatabase.DirectPExecute(
-        "UPDATE account SET online = 0 WHERE online > 0 "
-        "AND id IN (SELECT acctid FROM realmcharacters WHERE realmid = '%d')", realmID);
+    /// @TODO: Multirealm support (Or just drop `online` from auth?)
+    LoginDatabase.DirectExecute("UPDATE account SET online = 0 WHERE online <> 0");
 
     CharacterDatabase.DirectExecute("UPDATE characters SET online = 0 WHERE online <> 0");
 
