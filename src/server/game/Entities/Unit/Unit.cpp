faa281da77b326048c9ed585d5b1f38f7ddbec80
Ashley <saeshyls@gmail.com>
2013-12-08 03:22:24 -0500

    Prevent deserter from being removed by aura resets

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -4278,7 +4278,7 @@ void Unit::RemoveAllNegativeAuras()
         Aura const* aura = aurApp->GetBase();
         if ((!(aura->GetSpellInfo()->AttributesEx4 & SPELL_ATTR4_UNK21) && !aura->IsPassive()
             && (!aurApp->IsPositive() || !(aura->GetSpellInfo()->AttributesEx3 & SPELL_ATTR3_DEATH_PERSISTENT)))
-            && !aurApp->IsPositive() && aura->GetSpellInfo()->Id != 7267)
+            && !aurApp->IsPositive() && aura->GetSpellInfo()->Id != 7267 && aura->GetSpellInfo()->Id != 26013)
             RemoveAura(iter);
         else
             ++iter;
