4e6c6f62414825e92ddb7ac60319a14066f2a903
Ashley <saeshyls@gmail.com>
2014-10-15 20:38:50 -0400

    RIP TC/MaNGOS

--- src/server/game/AI/ScriptedAI/ScriptedCreature.cpp
+++ src/server/game/AI/ScriptedAI/ScriptedCreature.cpp
@@ -1,10 +1,3 @@
-/* Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
- *
- * Thanks to the original authors: ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
- *
- * This program is free software licensed under GPL version 2
- * Please see the included DOCS/LICENSE.TXT for more information */
-
 #include "ScriptPCH.h"
 #include "Item.h"
 #include "Spell.h"
--- src/server/game/AI/ScriptedAI/ScriptedGossip.h
+++ src/server/game/AI/ScriptedAI/ScriptedGossip.h
@@ -1,10 +1,3 @@
-/* Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
- *
- * Thanks to the original authors: ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
- *
- * This program is free software licensed under GPL version 2
- * Please see the included DOCS/LICENSE.TXT for more information */
-
 #ifndef SC_GOSSIP_H
 #define SC_GOSSIP_H
 
